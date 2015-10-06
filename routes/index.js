f78805d7320adf255bbc5e5a2137d966c6b42ac3
Robert Thuleen <rob.thuleen@coachlogix.com>
2015-10-06 13:17:44 -0700

    Added usage info to index view

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { 
+      title: 'RackSlack', 
+      info: 'A generic RackSpace Cloud Monitoring Alert integration with Slack',
+      usage: 'To use, congigure your Rackspace Cloud Monitoring nofication plan to send a webhook to ' +
+              'https://rackslack.herokuapp.com/alert/<slackTeamId>/<slackChannelId/<slackIntegrationId>'
+    });
 });
 
 module.exports = router;
