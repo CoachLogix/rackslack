b0276e0e290d7d239c33d1c07af33c7f018d3bc8
Robert Thuleen <rob.thuleen@coachlogix.com>
2015-10-06 13:19:22 -0700

    Fixed typo

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { 
       title: 'RackSlack', 
       info: 'A generic RackSpace Cloud Monitoring Alert integration with Slack',
-      usage: 'To use, congigure your Rackspace Cloud Monitoring nofication plan to send a webhook to ' +
+      usage: 'To use, configure your Rackspace Cloud Monitoring nofication plan to send a webhook to ' +
               'https://rackslack.herokuapp.com/alert/<slackTeamId>/<slackChannelId/<slackIntegrationId>'
     });
 });
